197c3d033bdb555953e2bc15e97be7873cedacce
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-16 14:22:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("inserisci il primo numero");
+string number1 = Console.ReadLine();
+Console.WriteLine("inserisci il secondo numero");
+string number2 = Console.ReadLine();
+
+if (int.Parse(number1) > int.Parse(number2))
+{
+    Console.WriteLine("Il numero maggiore è: " + number1);
+}
+else if (int.Parse(number1) < int.Parse(number2))
+{
+    Console.WriteLine("Il numero maggiore è: " + number2);
+} else
+{
+    Console.WriteLine("I numeri sono uguali");
+}
+Console.WriteLine();
+
+
